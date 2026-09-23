2e051d7d9b2ac2191a75008220c250044e432de3
Léo Belzile <belzilel@gmail.com>
2026-09-23 16:28:37 -0400

    Traduire code

--- code/MATH60604_4h_proportions.R
+++ code/MATH60604_4h_proportions.R
@@ -1,14 +1,19 @@
 # Régression logistique pour données binomiales
 # taux de succès aux examens pratique de conduite en Grande-Bretagne
 data(gbconduite, package = "hecmodstat")
-gbconduite$taille <- factor(cut(gbconduite$total, c(0, 500, 1000, Inf)), 
-                         labels = c("petit","moyen","grand"))
+gbconduite$taille <- factor(
+  cut(gbconduite$total, c(0, 500, 1000, Inf)),
+  labels = c("petit", "moyen", "grand")
+)
 gbconduite$region <- relevel(factor(gbconduite$region), "London")
 
 with(gbconduite, table(region, taille))
 # Argument est succès, échec
-mod_gbconduite <- glm(cbind(reussite, total-reussite) ~ sexe + region + taille,
-          data=gbconduite, family=binomial(link="logit"))
+mod_gbconduite <- glm(
+  cbind(reussite, total - reussite) ~ sexe + region + taille,
+  data = gbconduite,
+  family = binomial(link = "logit")
+)
 summary(mod_gbconduite)
 exp(cbind(coef(mod_gbconduite), confint(mod_gbconduite)))
 # Prédiction du modèle logistique - multiplier par taille!
@@ -18,11 +23,13 @@ predlogistic <- fitted(mod_gbconduite) * gbconduite$total
 # Cette sortie donnera la probabilité de succès
 # pour obtenir le dénombrement, multiplier par le nombre d'essais
 
-
-data(crash, package = "hecstatmod")
-mod_crash <- glm(cbind(ndeath, popn-ndeath) ~ time + year, family=binomial, data=crash)
-summary(mod_crash)
-expit <- function(x){ 1/(1+exp(-x))}
-# Estimated base rate per 100 000 inhabitants
-# is probability of death ("success") times 10^5
-expit(coef(mod_crash)[1])*1e5
+data(accident, package = "hecmodstat")
+mod_accidents_USA <- glm(
+  cbind(nmorts, popn - nmorts) ~ moment + annee,
+  family = binomial,
+  data = accident
+)
+summary(mod_accidents_USA)
+# Taux estimé par habitant
+# est probabilité de décès fois 10^5
+plogis(coef(mod_accidents_USA)[1]) * 1e5
